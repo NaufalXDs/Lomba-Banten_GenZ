b5f72c39afef5ec6610300d0c6862ef0df90e81b
NavDev <naufaldes07@gmail.com>
2024-04-26 16:36:05 +0700

    gitinore env

--- lib/variable-file/dev-env.h
+++ lib/variable-file/dev-env.h
@@ -1,6 +1,4 @@
 #define NTP_SERVER "ntp.nict.jp"
 #define TIME_ZONE "JST-9"
-#define API_KEY "AIzaSyBN0PxufEc39YpjnakGQ7IAq4ILWM9yjx0"
-#define DATABASE_URL "https://smart-garden-e93ad-default-rtdb.asia-southeast1.firebasedatabase.app/"
 const char *ssid = "TP-Link_TEI";
 const char *password = "iotproject2";
