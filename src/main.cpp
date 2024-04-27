b8e05847cce7db4d55d1fce3786833d45a848c15
NavDev <naufaldes07@gmail.com>
2024-04-27 12:34:39 +0700

    add -> Blynk Lib and Dht 11

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,38 @@
 #include "ESP8266HTTPClient.h"
 #include "ESP8266WiFi.h"
 #include "dev-env.h"
+#include "Blynk.h"
+#include "BlynkSimpleEsp8266.h"
+#include "Adafruit_Sensor.h"
 
 /* Variable Deklar */
-#define DHT_PIN 2 //D4
-#define Relay_pin 0 //D3
+#define DHT_PIN 2   // D4
+#define Relay_pin 0 // D3
 #define Soil_Moisture A0
+#define Buzzer_pin 14 // D5
 
 #define DHT_TYPE DHT11
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 
+uint32_t delayMS;
 DHT_Unified dht(DHT_PIN, DHT_TYPE);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+BlynkTimer timer;
 
 void kelembapan()
 {
     int value = analogRead(Soil_Moisture);
     Serial.println(F("Kelembapan: "));
     Serial.println(value);
+    Blynk.virtualWrite(V0, value);
 
     if (value > 380)
     {
         digitalWrite(Relay_pin, HIGH);
         Serial.println("Relay status: ON");
+        noTone(Buzzer_pin); // Buzzer_pin will sound with a tone of 1000 Hz
         // display.setCursor(0, 63);
         display.print("Kelembapan: ");
         display.println(value);
@@ -39,62 +47,105 @@ void kelembapan()
     {
         digitalWrite(Relay_pin, LOW);
         Serial.println("Relay status: OFF");
+        tone(Buzzer_pin, 500); // Buzzer will sound with a tone of 500 Hz
+        // display.setCursor(0, 63);
+        display.print("Kelembapan: ");
+        display.println(value);
+    }
+    else if (value >= 300 && value <= 700)
+    {
+        digitalWrite(Relay_pin, HIGH);
+        Serial.println("Relay status: ON");
+        noTone(Buzzer_pin); // Buzzer_pin will sound with a tone of 1000 Hz
+        // display.setCursor(0, 63);
+        display.print("Kelembapan: ");
+        display.println(value);
+    }
+    else if (value > 700)
+    {
+        digitalWrite(Relay_pin, HIGH);
+        Serial.println("Relay status: ON");
+        noTone(Buzzer_pin); // Buzzer_pin will sound with a tone of 1000 Hz
         // display.setCursor(0, 63);
         display.print("Kelembapan: ");
         display.println(value);
     }
 }
 
-void temp()
-{
+void dht11() {
+    delay(delayMS);
     sensors_event_t event;
     dht.temperature().getEvent(&event);
     if (isnan(event.temperature))
     {
-        Serial.println(F("Error reading temperature!"));
+        Serial.println(F("Error reading temperature"));
     }
     else
     {
         Serial.print(F("Temperature: "));
         Serial.print(event.temperature);
         Serial.println(F("°C"));
-        display.clearDisplay();
-        display.setTextSize(1);
-        display.setTextColor(SSD1306_WHITE);
-        display.setCursor(0, 0);
-        display.print(F("Temperature: "));
-        display.print(event.temperature);
-        display.print(F("°C"));
-        display.display();
-        Serial.print(F("Value: "));
-        Serial.println(event.temperature);
+        Blynk.virtualWrite(V1, event.temperature);
+    }
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity))
+    {
+        Serial.println(F("Error reading humidity"));
+    }
+    else
+    {
+        Serial.print(F("Humidity: "));
+        Serial.print(event.relative_humidity);
+        Serial.println(F("%"));
+        Blynk.virtualWrite(V2, event.relative_humidity);
     }
 }
 
-
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
+    Blynk.begin(auth, ssid, password);
+    while (Blynk.connected() == false)
+    {
+        Blynk.connect();
         delay(1000);
-        Serial.println("Connecting to WiFi..");
     }
-    Serial.println("WiFi connected successfully!");
-    Serial.println(WiFi.localIP());
-
-    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D untuk 128x64
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3D))
+    { // Address 0x3D untuk 128x64
         Serial.println(F("SSD1306 allocation failed"));
-        for(;;);
+        for (;;)
+            ;
     }
-
-    dht.begin();
 
     pinMode(Relay_pin, OUTPUT);
+    pinMode(Buzzer_pin, OUTPUT);
     pinMode(Soil_Moisture, INPUT);
+    pinMode(DHT_PIN, INPUT);
+    timer.setInterval(1000L, kelembapan);
+    timer.setInterval(1000L, dht11);
+
+    digitalWrite(Buzzer_pin, LOW);
+    noTone(Buzzer_pin);
+
+    /* DHT22 Sensor */
+    dht.begin();
+
+    Serial.println("DHT22 Sensor");
+    sensor_t sensor;
+    dht.temperature().getSensor(&sensor);
+    dht.humidity().getSensor(&sensor);
+
+    Serial.println("Temp Sensor");
+    Serial.print(F("Sensor Temp")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+
+    Serial.println("Humidity Sensor");
+    Serial.print(F("Sensor Humidity")); Serial.print(sensor.resolution); Serial.println(F("%"));
 }
 
-void loop() {
+void loop()
+{
     kelembapan();
-    // temp();
+    dht11();
+    Blynk.run();
     delay(2000);
 }
