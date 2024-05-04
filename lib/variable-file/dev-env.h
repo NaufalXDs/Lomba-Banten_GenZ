9ace46d5129cd5e4f27d6e91590a949ba983b9af
NavDev <naufaldes07@gmail.com>
2024-05-05 06:32:46 +0700

    edit -> ,gitinore & env file | reason: "Clean env"

--- lib/variable-file/dev-env.h
+++ lib/variable-file/dev-env.h
@@ -4,6 +4,10 @@ const char *ssid2 = "Host Home";
 const char *password2 = "Good193a";
 const char *ssid3 = "TP-Link_TEI";
 const char *password3 = "TP-Link_TEI";
+const char *ssid4 = "Infinix NOTE 30";
+const char *password4 = "88888888";
+const char *ssid5 = "Room";
+const char *password5 = "rikovalkik";
 #define BLYNK_TEMPLATE_ID "TMPL6pXzSxCJj"
 #define BLYNK_TEMPLATE_NAME "Monitoring Garden"
 #define BLYNK_AUTH "FpdiC0PsjzVN6N0fgSWLp4V7Iz_9qnEc"
--- lib/variable-file/production-env.h
+++ lib/variable-file/production-env.h
@@ -1,8 +1,13 @@
-#define SSID ""
-#define PASSWORD ""
-
-#define NTP_SERVER ""
-#define TIME_ZONE ""
-
-#define DHT_PIN 2
-#define DHT_TYPE DHT22
+const char *ssid1 = "";
+const char *password1 = "";
+const char *ssid2 = "";
+const char *password2 = "";
+const char *ssid3 = "";
+const char *password3 = "";
+const char *ssid4 = "";
+const char *password4 = "";
+const char *ssid5 = "";
+const char *password5 = "";
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME ""
+#define BLYNK_AUTH ""
