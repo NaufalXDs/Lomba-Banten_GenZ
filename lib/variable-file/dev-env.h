0c997fb5ce19855f5e10cf459f9f3b5117ed22b4
NFLDXD#0011 <68685792+NaufalXDs@users.noreply.github.com>
2024-05-05 06:36:42 +0700

    Update dev-env.h

--- lib/variable-file/dev-env.h
+++ lib/variable-file/dev-env.h
@@ -1,9 +1,13 @@
-#define NTP_SERVER "ntp.nict.jp"
-#define TIME_ZONE "JST-9"
-const char *ssid = "Host Home";
-const char *password = "Good193a";
-// const char *ssid = "TP-Link_TEI";
-// const char *password = "iotproject2";
-#define BLYNK_TEMPLATE_ID "TMPL6pXzSxCJj"
-#define BLYNK_TEMPLATE_NAME "Monitoring Garden"
-char auth[] = "4zPrwIbsaGy4BNuLQ0svLElBvRzEjxSa";
+const char *ssid1 = "";
+const char *password1 = "";
+const char *ssid2 = "";
+const char *password2 = "";
+const char *ssid3 = "";
+const char *password3 = "";
+const char *ssid4 = "";
+const char *password4 = "";
+const char *ssid5 = "";
+const char *password5 = "";
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME ""
+#define BLYNK_AUTH ""
