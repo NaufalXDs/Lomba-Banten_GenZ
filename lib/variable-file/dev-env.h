87e6749f39e1aacc056e1eefaf062f4d2433aa48
NFLDXD#0011 <68685792+NaufalXDs@users.noreply.github.com>
2024-05-05 06:36:15 +0700

    Update dev-env.h

--- lib/variable-file/dev-env.h
+++ lib/variable-file/dev-env.h
@@ -1,13 +1,13 @@
-const char *ssid1 = "goods";
-const char *password1 = "12345678";
-const char *ssid2 = "Host Home";
-const char *password2 = "Good193a";
-const char *ssid3 = "TP-Link_TEI";
-const char *password3 = "TP-Link_TEI";
-const char *ssid4 = "Infinix NOTE 30";
-const char *password4 = "88888888";
-const char *ssid5 = "Room";
-const char *password5 = "rikovalkik";
-#define BLYNK_TEMPLATE_ID "TMPL6pXzSxCJj"
-#define BLYNK_TEMPLATE_NAME "Monitoring Garden"
-#define BLYNK_AUTH "FpdiC0PsjzVN6N0fgSWLp4V7Iz_9qnEc"
+const char *ssid1 = "";
+const char *password1 = "";
+const char *ssid2 = "";
+const char *password2 = "";
+const char *ssid3 = "";
+const char *password3 = "";
+const char *ssid4 = "";
+const char *password4 = "";
+const char *ssid5 = "";
+const char *password5 = "";
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME ""
+#define BLYNK_AUTH ""
